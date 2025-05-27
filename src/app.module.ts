6c5bf018693e889dfa27084b116214b8daff17db
Juscilan Moreto <juscilan@gmail.com>
2025-05-27 11:49:37 -0300

    chore: add transactions files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Only for development
+    }),
+    TransactionsModule,
+  ],
 })
 export class AppModule {}
